d0f3f9257ad6ae0ac009c0b7270743748115d974
johannesm <lsgmathopo@gmail.com>
2025-07-31 23:54:50 +0200

    changed to Practice will make me perfect)

--- garden_advice.js
+++ garden_advice.js
@@ -1,5 +1,5 @@
 // Hardcoded values for the season and plant type
-// More advise to be added to clearify
+// Practice will make me perfect
 let season = "summer"; // TODO: Replace with prompt() to allow user interaction.
 let plantType = "flower"; // TODO: Replace with prompt() to allow user interaction.
 
